d810d6e0cb91d0dd6c95416f5e4b51b54a879525
Satoshi Ohki <roothybrid7@gmail.com>
2012-04-20 14:08:07 +0900

    Unbound: change to give compile options in ARGV

--- Library/Formula/unbound.rb
+++ Library/Formula/unbound.rb
@@ -5,11 +5,26 @@ class Unbound < Formula
   url 'http://www.unbound.net/downloads/unbound-1.4.16.tar.gz'
   sha256 'fb71665851eb11d3b1ad5dd5f9d7b167e0902628c06db3d6fc14afd95cc970fa'
 
+  def options
+  [
+    ["--with-libevent", "Build with libevent support."]
+  ]
+  end
+
   depends_on 'ldns'
-  depends_on 'libevent'
+  depends_on 'libevent' if ARGV.include? '--with-libevent'
 
   def install
-    system "./configure", "--disable-gost", "--prefix=#{prefix}", "--with-libevent"
+    args = [
+      "--disable-gost",
+      "--prefix=#{prefix}"
+    ]
+
+    if ARGV.include? "--with-libevent"
+      args << "--with-libevent=#{HOMEBREW_PREFIX}"
+    end
+
+    system "./configure", *args
     system "make install"
   end
 end
